ee339557de03eb31c7959b2f96668c99af3b07d2
Scott Starks <esstarks@hotmail.com>
2026-03-04 11:58:36 -0700

    text_to_textnode and markdown_to_blocks functions added

--- src/test_util.py
+++ src/test_util.py
@@ -62,5 +62,135 @@ class TestUtil(unittest.TestCase):
     )
     self.assertListEqual([("link", "https://www.fake.com")], matches)
 
+  def test_split_images(self):
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode(
+                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ],
+        new_nodes,
+    )
+
+  def test_split_links(self):
+    node = TextNode(
+      "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+      TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
+    self.assertListEqual(
+        [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ],
+        new_nodes,
+    )
+
+  def test_text_to_textnodes(self):
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    new_nodes = text_to_textnodes(text)
+    self.assertEqual(
+      [
+        TextNode("This is ", TextType.TEXT),
+        TextNode("text", TextType.BOLD),
+        TextNode(" with an ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word and a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" and an ", TextType.TEXT),
+        TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        TextNode(" and a ", TextType.TEXT),
+        TextNode("link", TextType.LINK, "https://boot.dev"),
+      ],
+      new_nodes
+    )
+
+  def test_text_to_textnodes_unordered(self):
+    text = "This is _text_ with [links](https://boot.dev), ![images](https://i.imgur.com/fJRm4Vk.jpeg), and **elements** in `random` order."
+    new_nodes = text_to_textnodes(text)
+    self.assertEqual(
+      [
+        TextNode("This is ", TextType.TEXT),
+        TextNode("text", TextType.ITALIC),
+        TextNode(" with ", TextType.TEXT),
+        TextNode("links", TextType.LINK, "https://boot.dev"),
+        TextNode(", ", TextType.TEXT),
+        TextNode("images", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        TextNode(", and ", TextType.TEXT),
+        TextNode("elements", TextType.BOLD),
+        TextNode(" in ", TextType.TEXT),
+        TextNode("random", TextType.CODE),
+        TextNode(" order.", TextType.TEXT)
+      ],
+      new_nodes
+    )
+
+  def test_text_to_textnodes_not_starting_with_text(self):
+    text = "**This text** doesn't start with `text`"
+    new_nodes = text_to_textnodes(text)
+    self.assertEqual(
+      [
+        TextNode("This text", TextType.BOLD),
+        TextNode(" doesn't start with ", TextType.TEXT),
+        TextNode("text", TextType.CODE)
+      ],
+      new_nodes
+    )
+
+  def test_markdown_to_blocks(self):
+    md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+    blocks = markdown_to_blocks(md)
+    self.assertEqual(
+        blocks,
+        [
+            "This is **bolded** paragraph",
+            "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+            "- This is a list\n- with items",
+        ],
+    )
+
+  def text_mardown_to_blocks_extra_newlines(self):
+    md = """
+This is **bolded** paragraph
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+
+- This is a list
+- with items
+"""
+    blocks = markdown_to_blocks(md)
+    self.assertEqual(
+        blocks,
+        [
+            "This is **bolded** paragraph",
+            "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+            "- This is a list\n- with items",
+        ],
+    )
+
 if __name__ == "__main__":
   unittest.main()
--- src/util.py
+++ src/util.py
@@ -25,3 +25,66 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):  
   links = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
   return links
+
+def split_nodes_image(old_nodes):
+  new_nodes = []
+  for node in old_nodes:    
+    if node.text_type is not TextType.TEXT:
+      new_nodes.append(node)
+    else:
+      images = extract_markdown_images(node.text)
+      if images == ():
+        new_nodes.append(node)
+      else:
+        current_text = node.text
+        for image in images:
+          image_alt, image_link = image
+          sections = current_text.split(f"![{image_alt}]({image_link})", 1)
+          if sections[0] != "":
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+          new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+          current_text = sections[1]
+        if current_text != "":
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+  return new_nodes
+        
+
+def split_nodes_link(old_nodes):
+  new_nodes = []
+  for node in old_nodes:
+    if node.text_type is not TextType.TEXT:
+      new_nodes.append(node)
+    else:
+      links = extract_markdown_links(node.text)
+      if links == ():
+        new_nodes.append(node)
+      else:
+        current_text = node.text
+        for link in links:
+          link_text, link_url = link
+          sections = current_text.split(f"[{link_text}]({link_url})", 1)
+          if sections[0] != "":
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+          new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+          current_text = sections[1]
+        if current_text != "":
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+  return new_nodes
+
+def text_to_textnodes(text):
+  textnode_to_process = TextNode(text, TextType.TEXT)
+  split_bold = split_nodes_delimiter([textnode_to_process], '**', TextType.BOLD)
+  split_italic = split_nodes_delimiter(split_bold, '_', TextType.ITALIC)
+  split_code = split_nodes_delimiter(split_italic, '`', TextType.CODE)
+  split_image = split_nodes_image(split_code)
+  result = split_nodes_link(split_image)
+  return result
+
+def markdown_to_blocks(markdown):
+  blocks = []
+  mardown_sections = markdown.split("\n\n")
+  for section in mardown_sections:
+    stripped_section = section.strip()
+    if stripped_section != "":
+      blocks.append(stripped_section)
+  return blocks
